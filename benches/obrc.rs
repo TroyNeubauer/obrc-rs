35661aa01b7ad4ec3e88425ae4c448bb9650caec
Troy Neubauer <troyneubauer@gmail.com>
2024-04-17 12:34:04 -0400

    log when tree dirty

--- benches/obrc.rs
+++ benches/obrc.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello world!");
+    let hash = Command::new("git")
+        .arg("rev-parse")
+        .arg("HEAD")
+        .output()
+        .unwrap();
+    let hash = String::from_utf8(hash.stdout).unwrap();
+
+    let tree_dirty = Command::new("git").arg("diff").output().unwrap();
+    let tree_dirty = if tree_dirty.stdout.is_empty() {
+        ""
+    } else {
+        "(tree dirty)"
+    };
+
+    println!("Hello world!: {} {tree_dirty}", hash.trim());
 }
+
