afbb9b75534a44ca4e9aef96fcd9e414af77744e
Troy Neubauer <troyneubauer@gmail.com>
2024-04-17 12:29:52 -0400

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use std::fmt::Write;
+use std::path::Path;
+
+pub struct ProcessedStation {
+    name: String,
+    min: f32,
+    avg_tmp: f32,
+    avg_count: usize,
+    max: f32,
+}
+
+pub fn solution(input_path: &Path) -> Vec<ProcessedStation> {
+    let mut stations: Vec<ProcessedStation> = vec![];
+    let data = std::fs::read_to_string(input_path).unwrap();
+    for line in data.lines() {
+        // `City of San Marino;30.0`
+        let Some((name, temp)) = line.split_once(';') else {
+            panic!("Line missing ; seperator! {line}");
+        };
+        let temp: f32 = temp.parse().unwrap();
+
+        match stations.iter_mut().find(|i| i.name == name) {
+            Some(station) => {
+                if temp < station.min {
+                    station.min = temp;
+                }
+                if temp > station.max {
+                    station.max = temp;
+                }
+
+                station.avg_tmp += temp;
+                station.avg_count += 1;
+            }
+            None => {
+                stations.push(ProcessedStation {
+                    name: name.to_owned(),
+                    min: temp,
+                    avg_tmp: temp,
+                    avg_count: 1,
+                    max: temp,
+                });
+            }
+        }
+    }
+
+    stations.sort_unstable_by_key(|s| s.name.clone());
+
+    stations
+}
+
+pub fn format_results(stations: &[ProcessedStation]) -> String {
+    let mut out = String::new();
+    out.push_str("{");
+    for (i, station) in stations.iter().enumerate() {
+        let avg = station.avg_tmp / station.avg_count as f32;
+        let _ = write!(
+            &mut out,
+            "{}={:.1}/{:.1}/{:.1}",
+            station.name, station.min, avg, station.max
+        );
+        if i != stations.len() - 1 {
+            let _ = write!(&mut out, ", ");
+        }
+    }
+
+    out.push_str("}");
+    out
+}
+
+#[test]
+fn validate() {
+    use std::time::Instant;
+
+    let input_path = "/home/troy/Java/1brc/measurements.txt";
+    let expected_out_path = "/home/troy/Java/1brc/expected_out.txt";
+    let expected = std::fs::read_to_string(expected_out_path).unwrap();
+    let expected = expected.trim();
+
+    let start = Instant::now();
+    let out = solution(Path::new(input_path));
+    let time_taken = start.elapsed();
+    println!("Took: {time_taken:?}");
+    let formatted = format_results(&out);
+    pretty_assertions::assert_eq!(formatted, expected);
+}
